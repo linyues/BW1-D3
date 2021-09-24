f48c2c5b21ca7d1b137d2915dfcf36fcf1691c33
linyues <2694166721@qq.com>
2021-09-24 22:23:21 +0800

    section2 PracticeC  finished

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,39 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
-        //实现练习要求，并改写该行代码。
+        //实现练习要求，并改写该行代码
+        Map<String,Integer> resultMap = new HashMap<>();
 
-        return null;
+        for (String s : collection1) {
+
+            if(resultMap.containsKey(s)){
+                resultMap.put(s,resultMap.get(s)+1);
+            }
+            else{
+                resultMap.put(s,1);
+            }
+
+            String[] temp = s.split("-|:|\\[|\\]");
+            if(temp.length>1){
+                if (resultMap.containsKey(temp[0])) {
+                    resultMap.put(temp[0], resultMap.get(temp[0]) + Integer.parseInt(temp[1]));
+                    resultMap.remove(s);
+                }
+                else {
+                    resultMap.put(temp[0], Integer.parseInt(temp[1]));
+                    resultMap.remove(s);
+                }
+            }
+        }
+
+        return resultMap;
     }
+
+
+
 }
