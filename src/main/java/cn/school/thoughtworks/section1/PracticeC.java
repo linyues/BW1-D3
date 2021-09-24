45b1fdfbfd61c6e132817a3a07913d2030c2f309
linyues <2694166721@qq.com>
2021-09-24 15:25:39 +0800

    PracticeC 用了另一种方法

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -2,10 +2,12 @@ package cn.school.thoughtworks.section1;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return new PracticeA().collectSameElements(collection1, collection2.get("value"));
+        //return new PracticeA().collectSameElements(collection1, collection2.get("value"));
+        return collection1.stream().filter(e -> collection2.get("value").contains(e)).collect(Collectors.toList());
     }
 }
