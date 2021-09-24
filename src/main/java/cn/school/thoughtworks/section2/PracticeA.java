65388122efef5919da5a6fcce346c96b4b2ab57a
linyues <2694166721@qq.com>
2021-09-24 16:11:41 +0800

    section2 PracticeA finished

--- src/main/java/cn/school/thoughtworks/section2/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeA.java
@@ -1,12 +1,25 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeA {
     Map<String,Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
+        Map<String,Integer> count =new HashMap<>();
+        for(String e:collection1){
+            if(count.containsKey(e)){
+                count.put(e,count.get(e)+1);
+            }
+            else{
+                count.put(e,1);
+            }
+        }
+        return count;
 
-        return null;
+
+
     }
+
 }
